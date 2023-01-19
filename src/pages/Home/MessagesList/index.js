5c7192b5fa60cbc64fc156dfd081b3f0e63255f8
Sébastien Lampazona <sebastien@oclock.io>
2023-01-19 22:44:52 +0100

    feat(Virtuoso): ajout de react virtuoso

--- src/pages/Home/MessagesList/index.js
+++ src/pages/Home/MessagesList/index.js
@@ -1,19 +1,35 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import Message from 'src/pages/Home/MessagesList/Message';
+import { Virtuoso } from 'react-virtuoso';
 
 import './styles.scss';
 
+// TODO Stick to Bottom : https://virtuoso.dev/stick-to-bottom/
 function MessagesList() {
   return (
-    <div className="chat-messages-list">
-      <Message message="Hello" />
-      <Message message="Hello" isMine />
-      <Message message="Comment vas-tu ?" isMine />
-      <Message message="Et bien ça va bien merci et toi ?" />
-      <Message message="Super nickel👌" isMine />
-      <Message message="I love cheese, especially cheese and wine cottage cheese. Cauliflower cheese who moved my cheese lancashire blue castello feta cheese strings danish fontina babybel. Smelly cheese danish fontina caerphilly camembert de normandie stinking bishop boursin cheese on toast babybel. Bocconcini melted cheese cheddar caerphilly cow halloumi cream cheese." />
-      <Message message="Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac suscipit leo. Carpe diem vulputate est nec commodo rutrum. Pellentesque mattis convallis nisi eu and I ain’t stoppin until the swear jar’s full. Ut rhoncus velit at mauris interdum, fringilla dictum neque rutrum. Curabitur mattis odio at erat viverra lobortis." isMine />
-    </div>
+    <Virtuoso
+      className="chat-messages-list"
+      data={[
+        { isMine: false, text: 'Hello' },
+        { isMine: true, text: 'Hello' },
+        { isMine: true, text: 'Comment vas-tu ?' },
+        { isMine: false, text: 'Et bien ça va bien merci et toi ?' },
+        { isMine: true, text: 'Super nickel👌' },
+        {
+          isMine: false,
+          text: 'I love cheese, especially cheese and wine cottage cheese. Cauliflower cheese who moved my cheese lancashire blue castello feta cheese strings danish fontina babybel. Smelly cheese danish fontina caerphilly camembert de normandie stinking bishop boursin cheese on toast babybel. Bocconcini melted cheese cheddar caerphilly cow halloumi cream cheese.',
+        },
+        {
+          isMine: true,
+          text: 'Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac suscipit leo. Carpe diem vulputate est nec commodo rutrum. Pellentesque mattis convallis nisi eu and I ain’t stoppin until the swear jar’s full. Ut rhoncus velit at mauris interdum, fringilla dictum neque rutrum. Curabitur mattis odio at erat viverra lobortis.',
+        },
+      ]}
+      initialTopMostItemIndex={6}
+      itemContent={(index, message) => (
+        <Message message={message.text} isMine={message.isMine} />
+      )}
+    />
   );
 }
 export default MessagesList;
